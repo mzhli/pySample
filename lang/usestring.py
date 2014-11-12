f98c2e842a88adf77db9599ab8f470b00930023d
mzhli <mzhli@telenav.cn>
2014-11-12 11:55:11 +0800

    Add function pickMatchedStrings

--- lang/usestring.py
+++ lang/usestring.py
@@ -1,8 +1,19 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 2014年11月12日
 
 @author: mzhli
 '''
 
+import re
+
+def pickMatchedStrings(pattern, listInputString):
+    result = [ astr for astr in listInputString if re.search(pattern, astr) ]
+    return result
+
 if __name__ == '__main__':
-    pass
+    inputdata = ["mzhli2000", "mzhli", "zuludic@hotmail.com", "mzhli@telenav.cn"]
+    patterns = ['^mzhli', '\d', '@']
+    print "Input strings: %s" % (str(inputdata))
+    for pattern in patterns:
+        print "Pattern '%s' => %s" % (pattern, pickMatchedStrings(pattern, inputdata))
